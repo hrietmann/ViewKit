4bf80273162ae71d40359d1ca232039e997a3637
Hans Rietmann <hrietmann@icloud.com>
2021-06-12 17:42:39 +0200

    New Views : ✅ HomeView (tab bar view that holds multiple pages)

---
+++ Sources/Views/RoundedCorners.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedCorners.swift
+//  qsdfghjklm
+//
+//  Created by Hans Rietmann on 11/06/2021.
+//
+
+import SwiftUI
+
+fileprivate struct RoundedCorner: Shape {
+
+    fileprivate var radius: CGFloat = .infinity
+    fileprivate var corners: UIRectCorner = .allCorners
+
+    
+    fileprivate func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+internal extension View {
+    func corner(radius: CGFloat, to corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+struct RoundedCorner_Previews: PreviewProvider {
+    static var previews: some View {
+        Rectangle()
+            .corner(radius: 20, to: [.topLeft, .topRight])
+    }
+}
